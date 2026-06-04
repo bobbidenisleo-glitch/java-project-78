f58b25d4265e7ccbef6aeab54ecc18d83a4d2bf8
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-06-04 16:27:48 +0300

    Add AssertJ dependency and fix test file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,6 +13,7 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
