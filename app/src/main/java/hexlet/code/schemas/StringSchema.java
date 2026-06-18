5062b7848cec8e6dbabfcfa7881cf7e7969ff1aa
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-06-18 11:42:37 +0300

    Force reset StringSchema file to clear cache

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -3,16 +3,19 @@ package hexlet.code.schemas;
 public class StringSchema extends BaseSchema<String> {
 
     public StringSchema required() {
+        validators.removeIf(p -> p.toString().contains("required"));
         validators.add(s -> s != null && !s.isEmpty());
         return this;
     }
 
     public StringSchema minLength(int length) {
+        validators.removeIf(p -> p.toString().contains("minLength"));
         validators.add(s -> s != null && s.length() >= length);
         return this;
     }
 
     public StringSchema contains(String substring) {
+        validators.removeIf(p -> p.toString().contains("contains"));
         validators.add(s -> s != null && s.contains(substring));
         return this;
     }
