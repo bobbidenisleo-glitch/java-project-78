8064f1cc7cf8e458aa3031ee22c716fa7c5532da
bobbidenisleo-glitch <bobbidenisleo@gmail.com>
2026-06-18 11:35:35 +0300

    Add testStringValidator to override old CI cache

---
+++ app/src/test/java/hexlet/project/ValidatorTest.java
@@ -0,0 +1,15 @@
+package hexlet.project;
+
+import hexlet.code.Validator;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidatorTest {
+
+    @Test
+    void testStringValidator() {
+        Validator v = new Validator();
+        var schema = v.string().required().minLength(10).minLength(4);
+        assertTrue(schema.isValid("hexlet"));
+    }
+}
